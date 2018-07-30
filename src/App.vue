a301e0665a05cf2266d1a755e8783458d63cda56
Donely Gunn <dgoon@decentralizejsutice.com>
2018-07-29 23:59:45 -0700

    Changed Entire Stack Changed stack to vue js via cli Need to port over code from old development branch

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-card no-body>
+    <b-tabs pill card vertical>
+      <b-tab title="Create" active>
+        Tab Contents 1
+      </b-tab>
+      <b-tab title="Spend">
+        Tab Contents 2
+      </b-tab>
+      <b-tab title="Sign">
+        Tab Contents 3
+      </b-tab>
+      <b-tab title="Settings">
+        Tab Contents 3
+      </b-tab>
+    </b-tabs>
+  </b-card>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
