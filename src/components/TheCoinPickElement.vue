2274202f796163382f4b55bff85bcd63245f34cf
Donely Gunn <dgoon@decentralizejsutice.com>
2018-08-01 03:16:32 -0700

    fix

--- src/components/TheCoinPickElement.vue
+++ src/components/TheCoinPickElement.vue
@@ -1,21 +1,20 @@
 <template>
-  <div >
-
-  <p class="h2" style="padding-top: 2%;"> Pick Your Crypto:<p>
-  <div  class="row text-center justify-content-center mt-5">
-    <!--Btc-->
-    <div id="Ethereum" class="col-3 mx-auto d-block">
-        <img src="../assets/eth.svg" alt="Ethereum" style="width: 60%;">
+  <div v-if="show">
+    <p class="h2" style="padding-top: 2%;"> Pick Your Crypto:<p>
+    <div  class="row text-center justify-content-center mt-5">
+      <!--Btc-->
+      <div id="Ethereum" class="col-3 mx-auto d-block">
+          <img src="../assets/eth.svg" alt="Ethereum" style="width: 60%;">
+      </div>
+      <!--Btc-->
+      <div id="Bitcoin" class="col-3 coinPick  mx-auto d-block" >
+          <img src="../assets/bitcoin.svg" alt="Bitcoin" style="width: 100%;">
+      </div>
+      <!--Ltc-->
+      <div id="Litecoin" class="col-3 mx-auto d-block" >
+          <img src="../assets/litecoin.svg" alt="Litecoin" style="width: 100%;">
+      </div>
     </div>
-    <!--Btc-->
-    <div id="Bitcoin" class="col-3 coinPick  mx-auto d-block" >
-        <img src="../assets/bitcoin.svg" alt="Bitcoin" style="width: 100%;">
-    </div>
-    <!--Ltc-->
-    <div id="Litecoin" class="col-3 mx-auto d-block" >
-        <img src="../assets/litecoin.svg" alt="Litecoin" style="width: 100%;">
-    </div>
-  </div>
 </div>
 </template>
 
@@ -23,8 +22,8 @@
 export default {
   name: 'TheCoinPickElement',
   computed: {
-    products () {
-      return this.$store.state.products
+    show () {
+      return this.$store.state.showOpeningCoinPick
     }
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -5,11 +5,6 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    products: [
-      {name: 'Banana Skin', price: 20},
-      {name: 'Shiny Star', price: 40},
-      {name: 'Green Shells', price: 60},
-      {name: 'Red Shells', price: 80}
-    ]
+    showOpeningCoinPick: false
   }
 })
