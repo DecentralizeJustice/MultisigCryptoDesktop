42f6304fc910079e7e85a0871b726356780dcbca
Donely Gunn <dgoon@decentralizejsutice.com>
2018-09-09 22:52:19 -0700

    Setup Nested Components

---
+++ components/TheSetupElement/TheSetupView.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <TheSetupDeviceIntro></TheSetupDeviceIntro>
+</template>
+
+<script>
+import TheSetupDeviceIntro from '~/components/TheSetupElement/TheDeviceorTools.vue'
+export default {
+  name: 'TheSetupView',
+  components: {
+      TheSetupDeviceIntro
+    },
+  
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
