04cd217446f320d6248c32497918bd34f5ca7c8d
Donely Gunn <dgoon@decentralizejsutice.com>
2018-09-21 17:13:01 -0700

    Working on Final Plan Promise Functions

--- components/TheSetupElement/DeviceInfo/FinalPlan.vue
+++ components/TheSetupElement/DeviceInfo/FinalPlan.vue
@@ -3,11 +3,10 @@
    <v-container  fluid fill-height>
       <v-layout>
         <v-flex xs12 fluid>
-          <v-card flat>
+          <v-card flat row wrap>
             <v-img
               src="plan.jpg"
               aspect-ratio="5"
-
             ></v-img>
 
             <v-card-title primary-title class="justify-center" >
@@ -15,23 +14,39 @@
                 <h1 >Confirm Final Plan</h1>
               </div>
             </v-card-title>
-            <v-flex xs4>
+
+            <v-layout xs12  wrap justify-space-around row fill-height>
+
+            <v-flex xs12 sm3>
               <v-card>
                 <v-toolbar color="black" >
                   <v-layout row wrap align-center>
-                    <v-flex class="text-xs-center">
-                      <v-toolbar-title>Laptop 1</v-toolbar-title>
+                    <v-flex headline class="text-xs-center">
+                      Laptop 1
                     </v-flex>
                   </v-layout>
                 </v-toolbar>
-
                 <v-card-title>
                    <div>
-                     <h3>Number of Hardware Wallets:</h3>
+                     <h3 >Number of Hardware Wallets:</h3>
                    </div>
                  </v-card-title>
                 </v-card>
               </v-flex>
+
+              <v-flex xs12 sm3 pb-3>
+                <v-card  hover color='clear'>
+                   <v-toolbar>
+                     <v-layout row wrap align-center>
+                       <v-flex headline class="text-xs-center">
+                         Cellphone 1
+                       </v-flex>
+                     </v-layout>
+                   </v-toolbar>
+                   <v-img src="/trezor.jpg" aspect-ratio="1.5"></v-img>
+                </v-card>
+              </v-flex>
+            </v-layout>
               </v-card>
             </v-flex>
       </v-layout>
@@ -48,28 +63,39 @@ export default {
       this.$emit('pickOption',option)
     },
     createobjects (choiceArray){
-
       if(choiceArray[1]!==0){
         for (let i = 0; i < choiceArray[1]; i++) {
-          this.laptops['lap'+'i'] =[]
+          this.laptops['lap'+i] =[]
         }
       }
-      if(choiceArray[1]!==0){
+      if(choiceArray[2]!==0){
         for (let i = 0; i < choiceArray[2]; i++) {
-          this.cellphones['cell'+'i'] =[]
+          this.cellphones['cell'+i] =[]
         }
       }
-    }
+      //alert(this.cellphones['cell1'])
+    },
+    showOff (func) {
+    return new Promise(
+        function (resolve) {
+            //this.createobjects(this.choiceArray)
+            func
+            resolve(this.cellphones['cell']+'l')
+        }
+    )
+  },
+
   },
   data: function () {
     return {
       laptops: {},
       cellphones: {},
-
       }
     },
     beforeMount(){
-      this.createobjects(this.choiceArray)
+      this.showOff(this.createobjects(this.choiceArray)).then(result =>
+        alert(result) // 1
+      )
    },
 
 
