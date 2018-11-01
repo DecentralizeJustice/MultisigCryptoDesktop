2344e6063cdc2597c3c4a5bff1ea7e79172314cc
Donely Gunn <dgoon@decentralizejsutice.com>
2018-11-01 00:17:38 -0700

    Need to test Trezor

--- assets/trezorConnect/Main.js
+++ assets/trezorConnect/Main.js
@@ -4,11 +4,39 @@ import { supportedCoins as coins } from '~/assets/supportedCoins.js'
 export { getPublicKey }
 async function getPublicKey () {
   let xpub = {}
+  let bundleVar = await getBundle(coins)
+  let xpubBundle = await TrezorConnect.getPublicKey({
+    bundle: bundleVar
+  })
+  xpub = parsePayload(xpubBundle.payload)
+  return xpub
+}
+
+function getBundle (coins) {
+  let bundle = []
   for (var prop in coins) {
-    xpub[prop] = await TrezorConnect.getPublicKey({
-      path: coins[prop].currentPath,
-      coin: prop
-    })
+    bundle.push({ 'path': coins[prop].currentPath })
+  }
+  return bundle
+}
+
+function parsePayload (payload) {
+  let xpub = {}
+  for (let i = 0; i < payload.length; i++) {
+    let ticker = findTicker(payload[i].serializedPath, coins)
+    xpub[ticker] = payload[i].xpub
   }
   return xpub
+}
+
+function findTicker (path, coins) {
+  path = 'm/' + path
+  let coin = ''
+  for (var prop in coins) {
+    if (coins[prop].currentPath === path) {
+      coin = prop
+      break
+    }
+  }
+  return coin
 }
