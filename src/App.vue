a6e75cfbb047f480c1e9724ae283a3f4a0412fed
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-03 11:54:33 -0800

    Working Repo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <router-view/>
+  <v-app  xs12 dark v-bind:style="styleObject" class='fullbackground'>
+    <router-view/>
+  </v-app>
+
 </template>
+<script>
+export default {
+  data () {
+    return {
+      styleObject: {
+        'background-image': 'url("https://images.pexels.com/photos/733475/pexels-photo-733475.jpeg?cs=srgb&dl=astronomy-cosmos-dark-733475.jpg&fm=jpg")'
+      }
+    }
+  }
+}
+</script>
 
-<style>
-
+<style scoped>
+.fullbackground {
+   background-position: center;
+   background-repeat: no-repeat;
+   background-size: cover;
+}
 </style>
