e5231acc7921509860cb898879a039ad29582123
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-03 11:18:18 -0800

    Added bacground

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <v-app  xs12 dark v-bind:style="styleObject" class='fullbackground'>
     <HelloWorld msg="Hot"/>
-  </div>
+  </v-app>
 </template>
 
 <script>
@@ -13,6 +12,21 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data () {
+  return {
+    styleObject: {
+      'background-image': 'url("https://images.pexels.com/photos/733475/pexels-photo-733475.jpeg?cs=srgb&dl=astronomy-cosmos-dark-733475.jpg&fm=jpg")'
+    }
   }
+},
 }
 </script>
+
+<style scoped>
+.fullbackground {
+   background-position: center;
+   background-repeat: no-repeat;
+   background-size: cover;
+}
+</style>
